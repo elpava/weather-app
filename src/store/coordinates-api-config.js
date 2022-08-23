cd8836b68ee9f4d07f52af8412db9244695ba26f
hadikman <hadikman@gmail.com>
2022-08-23 11:09:50 +0430

    delete unused codes

--- src/store/coordinates-api-config.js
+++
@@ -1,8 +0,0 @@
-// const API_KEY =
-//   'pk.eyJ1IjoiaGFkaWttYW4iLCJhIjoiY2twc2MybWt5MTF6czJxczR1ZGNqd3N6NSJ9.UA1qAc3pGYraATf9kGt5DQ';
-
-// let location = 'Tehran';
-
-// export const getCoordinatesURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?access_token=${API_KEY}`;
-
-// export const options = {method: 'GET', headers: {Accept: 'application/json'}};
